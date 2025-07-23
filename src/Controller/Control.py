c61c00d100298f07b74f2bb6e0dd800ef032ca70
marcosatc147 <221008300@aluno.unb.br>
2025-07-23 17:24:02 -0300

    implementa object avoidance em control

--- src/Controller/Control.py
+++ src/Controller/Control.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import urllib2
 import json
+import time
 from JoyStick import Joystick
 
 class Control(object):
@@ -10,12 +11,18 @@ class Control(object):
         self.__right_joystick = None
         self.__nao = nao
         self.__running = True
+        self.__last_obstacle_time = 0
 
     def __fetch_joystick_data(self):
-        response = urllib2.urlopen(self.__server_url)
-        data = json.load(response)
-        self.__left_joystick = Joystick(data.get('esquerdo', {}))
-        self.__right_joystick = Joystick(data.get('direito', {}))
+        """Método para obter dados do joystick"""
+        try:
+            response = urllib2.urlopen(self.__server_url)
+            data = json.load(response)
+            self.__left_joystick = Joystick(data.get('esquerdo', {}))
+            self.__right_joystick = Joystick(data.get('direito', {}))
+        except Exception, e:
+            print("Erro ao obter dados do joystick: {}".format(e))
+            raise
 
     def breakAll(self):
         self.__running = False
@@ -26,18 +33,37 @@ class Control(object):
             try:
                 self.__fetch_joystick_data()
 
-                x_right, y_right = 0, 0
-                x_left, y_left = 0, 0
-                if not self.__right_joystick.is_using:
+                x_right, y_right = 0.0, 0.0
+                x_left, y_left = 0.0, 0.0
+                
+                if self.__right_joystick and not self.__right_joystick.is_using:
                     x_right, y_right = self.__right_joystick.direction_vector
+                    
+                    # Reduz velocidade se obstáculo detectado recentemente
+                    if time.time() - self.__last_obstacle_time < 3:
+                        x_right *= 0.7
+                        y_right *= 0.7
 
-                if not self.__left_joystick.is_using:
+                if self.__left_joystick and not self.__left_joystick.is_using:
                     x_left, y_left = self.__left_joystick.direction_vector
 
+                # Verificar obstáculos com movimento inteligente
+                if getattr(self.__nao, '_NAO__obstacle_detected', False):
+                    # Se estiver tentando ir PARA FRENTE (y_right > 0), bloqueia
+                    if y_right > 0.1:  # Limiar de sensibilidade
+                        self.__nao.move(0.0, 0.0)  # Para movimento frontal
+                        self.speak("Obstacle ahead")
+                    # Permite movimento para trás ou laterais
+                    elif y_right < -0.1 or abs(x_right) > 0.1:
+                        self.__nao.move(x_right, y_right)  # Permite recuar ou girar
+                    self.__last_obstacle_time = time.time()
+                else:
+                    # Movimento normal sem obstáculos
+                    self.__nao.move(x_right, y_right)
+                    self.__nao.look(x_left, y_left)
 
-                self.__nao.move(x_right, y_right)
-                self.__nao.look(x_left, y_left)
+                time.sleep(0.05)
 
-
-            except Exception as e:
-                print("Failed to retrieve joystick data: %s" % str(e))
+            except Exception, e:
+                print("Erro no loop principal: {}".format(e))
+                time.sleep(1)
